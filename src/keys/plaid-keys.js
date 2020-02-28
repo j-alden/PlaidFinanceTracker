2f7b7039660723013e39ecd04f182316e0fbc442
eyedeaman <jholt25@gmail.com>
2020-02-28 06:38:34 -0600

    updates

--- src/keys/plaid-keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  clientId: '5dab00bb1a28650013d270aa',
-  publicKey: 'e6c81eb976a8bd8d1ee0b3846eafb9',
-  secret: '24f4c156637293aa5fe509448ef19d',
-  publicToken: 'public-development-42ad1485-682d-4914-b1bd-4e181ae690d1' // Chase
-};
